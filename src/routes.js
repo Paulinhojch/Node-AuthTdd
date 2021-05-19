88037b6a15bd35bf583efc7114e407e3a278df95
Paulo <paulo.juchimiuk@alobebe.com.br>
2021-05-19 18:03:49 -0300

    add Model User

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
 
-
-
+User.create({
+    name: "Paulo",
+    email: "paulo@alobebe.com.br",
+    password_hash: "21s21v2df3bf2b1f2"
+});
 
 module.exports = routes;
